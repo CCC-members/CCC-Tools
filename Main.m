0dcb7ec4e7f15b25b0d67f6261bfb51e63a221b7
Ariosky <ariosky.areces@neuroinformatics-collaboratory.org>
2020-11-22 19:50:05 +0800

    Starting tools project and Including get report tool

---
+++ Main.m
@@ -0,0 +1,17 @@
+%%
+%% Test all tools
+
+clear all;
+close all;
+clc;
+
+disp("-->> Starting test of tools.");
+addpath('make_reports');
+addpath('templates');
+
+%% Report tool
+Test_report;
+
+
+
+disp("-->> Tools test finished.");
